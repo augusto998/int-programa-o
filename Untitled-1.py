e1fcfe8867bfadeb85babffa20289ebdbd7a861e
augusto998 <augustomalaquiascarvalho101@gmail.com>
2025-08-19 07:55:59 -0300

    Add files via upload
    tarefa sobre calculo da media anual

---
+++ Untitled-1.py
@@ -0,0 +1,4 @@
+nota1=int(input("digite a primeira nota: "))
+nota2=int(input("digite a primeira nota: "))
+nfalta=int(60-(nota1*2+nota2*2))
+print("voce precisa tirar",(nfalta/2/3),"nos proximos 2 bimestres")
